8b30a81a2082153699c9b47cde04e168b4191635
mdfsar15 <57721986+mdafsar15@users.noreply.github.com>
2020-06-24 14:19:15 +0530

    [Afsar]Refactor. verifIcation url

--- src/main/java/com/bridgelabz/bookstore/utility/Utils.java
+++ src/main/java/com/bridgelabz/bookstore/utility/Utils.java
@@ -5,13 +5,13 @@ public class Utils {
 	public static final String SECRET_KEY = "SCH567";
 	public static final String ISSUER = "Bridgelabz";
 	public static final String SUBJECT = "Authentication";
-	public static final String VERIFICATION_URL = "http://localhost:8080/user/verify/";
+	public static final String VERIFICATION_URL = "http://localhost:8081/user/verify/";
 	public static final long REGISTRATION_EXP = (long) 3 * 60 * 60 * 1000;
-	public static final String SENDER_EMAIL_ID = "seemabaghel1997@gmail.com";
-	public static final String SENDER_PASSWORD = "seemabaghel@";
+	public static final String SENDER_EMAIL_ID = "mdafsaransari720@gmail.com";
+	public static final String SENDER_PASSWORD = "afsaralam1515";
 	public static final int BAD_REQUEST_RESPONSE_CODE = 400;
 	public static final int OK_RESPONSE_CODE = 200;
 	public static final int USER_AUTHENTICATION_EXCEPTION_STATUS = 401;
 	public static final int ALREADY_VERIFIED_EXCEPTION_STATUS = 201;
-	public static final String RESETPASSWORD_URL = "http://localhost:8080/user/resetpassword/";
+	public static final String RESETPASSWORD_URL = "http://localhost:8081/user/resetpassword/";
 }
