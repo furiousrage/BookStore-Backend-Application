608ae7701ee187a90716bca910561b7896eb0e75
MOHAN-B6695 <mohan.b600@gmail.com>
2020-06-27 12:07:55 +0530

    [Mohan]ADDED. Created book entity

---
+++ src/main/java/com/bridgelabz/bookstore/model/BookModel.java
@@ -0,0 +1,67 @@
+package com.bridgelabz.bookstore.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "book")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class BookModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "book_id")
+	private Long bookId;
+
+	@Column
+	@NotNull
+	private String bookName;
+
+	@Column
+	@NotNull
+	private int quantity;
+
+	@Column
+	@NotNull
+	private Double price;
+
+	@Column
+	@NotNull
+	private String authorName;
+
+	@CreationTimestamp
+	private LocalDateTime createdDateAndTime;
+	
+	@UpdateTimestamp
+	private LocalDateTime UpdatedDateAndTime;
+
+//	@Column
+//	private String image;
+
+	@Column
+	@NotNull
+	private String bookDetails;
+
+	@Column(nullable = false)
+	private boolean isVerfied;
+		
+}
