c02790cc432b629ef76856a6330793671efd3fd5
Your Name <nnchavadi2@gmail.com>
2020-06-29 01:00:26 +0530

    [Niranjanachavadi]ADDED two methods update book using update bookdto and delete book by id

--- src/main/java/com/bridgelabz/bookstore/controller/SellerController.java
+++ src/main/java/com/bridgelabz/bookstore/controller/SellerController.java
@@ -5,12 +5,15 @@ package com.bridgelabz.bookstore.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.bridgelabz.bookstore.dto.BookDto;
+import com.bridgelabz.bookstore.dto.UpdateBookDto;
 import com.bridgelabz.bookstore.exception.UserException;
 import com.bridgelabz.bookstore.response.Response;
 import com.bridgelabz.bookstore.service.SellerService;
@@ -32,4 +35,20 @@ public class SellerController {
 		Response addedbook = sellerService.addBook(newBook, token);
 		return new ResponseEntity<Response>(addedbook, HttpStatus.OK);
 	}
+	
+	
+	@PutMapping(value = "/updateBook", headers = "Accept=application/json")
+	public ResponseEntity<Response> updateBook(@RequestBody UpdateBookDto newBook, @RequestHeader("token") String token,Long bookId)
+			throws UserException {
+		Response addedbook = sellerService.updateBook(newBook, token, bookId);
+		return new ResponseEntity<Response>(HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/DeleteBook", headers = "Accept=application/json")
+	public ResponseEntity<Response> deleteBook( @RequestHeader("token") String token,Long bookId)
+			throws UserException {
+		Response addedbook = sellerService.deleteBook(token, bookId);
+		return new ResponseEntity<Response>(HttpStatus.OK);
+	}
+	
 }
