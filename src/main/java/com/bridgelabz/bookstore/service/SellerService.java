eb4b27d8066912bbbffe1d5ad615b2d21676e02c
Your Name <nnchavadi2@gmail.com>
2020-06-29 01:03:45 +0530

    [Niranjanchavadi]REFACTORED declared two method for delete , updatebook information

--- src/main/java/com/bridgelabz/bookstore/service/SellerService.java
+++ src/main/java/com/bridgelabz/bookstore/service/SellerService.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.bookstore.service;
 
 import com.bridgelabz.bookstore.dto.BookDto;
+import com.bridgelabz.bookstore.dto.UpdateBookDto;
 import com.bridgelabz.bookstore.exception.UserException;
 import com.bridgelabz.bookstore.model.BookModel;
 import com.bridgelabz.bookstore.response.Response;
@@ -8,5 +9,10 @@ import com.bridgelabz.bookstore.response.Response;
 public interface SellerService {
 
 	Response addBook(BookDto newBook, String token) throws UserException;
+
+	Response updateBook(UpdateBookDto newBook, String token,Long BookId) throws UserException;
+
+	Response deleteBook(String token, Long bookId);
+
 
 }
