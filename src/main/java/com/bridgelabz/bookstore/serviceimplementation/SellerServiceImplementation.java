769694cfdbd74d7c9d737f1f66d4806700ee78d3
Your Name <nnchavadi2@gmail.com>
2020-06-29 01:05:24 +0530

    [Niranjanchavadi]REFACTORED implemented two method for delete , updatebook infortion

--- src/main/java/com/bridgelabz/bookstore/serviceimplementation/SellerServiceImplementation.java
+++ src/main/java/com/bridgelabz/bookstore/serviceimplementation/SellerServiceImplementation.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.bookstore.serviceimplementation;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.bridgelabz.bookstore.dto.BookDto;
+import com.bridgelabz.bookstore.dto.UpdateBookDto;
 import com.bridgelabz.bookstore.exception.UserException;
 import com.bridgelabz.bookstore.model.BookModel;
 import com.bridgelabz.bookstore.repository.BookRepository;
@@ -47,6 +49,39 @@ public class SellerServiceImplementation implements SellerService {
 	     
 	}
 	 return new Response(HttpStatus.OK.value(),"Book Not Added Becoz Not Authoriized to add Book");
+}
+
+	@Override
+	public Response updateBook(UpdateBookDto newBook, String token,Long bookId) throws UserException {
+		long id = JwtGenerator.decodeJWT(token);
+		String role = userRepository.checkRole(id);
+		if(role.equals("SELLER")){
+			Optional<BookModel> book = bookRepository.findById(bookId);
+		BeanUtils.copyProperties(newBook,book.get());
+		book.get().setVerfied(false);
+		book.get().setUpdatedDateAndTime(LocalDateTime.now());
+		book.get().setCreatedDateAndTime(LocalDateTime.now());
+		book.get().isVerfied();
+		bookRepository.save(book.get());
+		elasticSearchService.updateBook(book.get());
+		return new Response(HttpStatus.OK.value(),"Book update Successfully Need to Verify");
+	     
+	}
+	 return new Response(HttpStatus.OK.value(),"Book Not updated Becoz Not Authoriized to add Book");
+	}
+	
+	
+	@Override
+	public Response deleteBook(String token, Long bookId) {
+		long id = JwtGenerator.decodeJWT(token);
+		String role = userRepository.checkRole(id);
+		if(role.equals("SELLER")){
+		bookRepository.deleteById(bookId);
+		elasticSearchService.deleteNote(bookId);
+		return new Response(HttpStatus.OK.value(),"Book deleted Successfully ");
+	     
+	}
+	 return new Response(HttpStatus.OK.value(),"Book Not deleted Becoz Not Authoriized to delete Book");
 }
 	
 	
